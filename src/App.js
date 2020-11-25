58011f8721aefa72c5202615fe95ee36e95f609d
ingan <isabelngan27@gmail.com>
2020-11-24 22:45:40 -0500

    home text animations

--- src/App.js
+++ src/App.js
@@ -248,7 +248,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/">
             <Container>
-              <Home home={home} />
+              <Home home={home} changeChapter={this.changeChapter} />
             </Container>
           </Route>
 
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -6,6 +6,7 @@ import "./style.css";
 import { Grid, Box } from '@material-ui/core';
 import { gsap } from "gsap";
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Scrollbar from 'smooth-scrollbar';
 gsap.registerPlugin(ScrollTrigger);
 
 
@@ -26,55 +27,69 @@ const Home = (props) => {
     const [chapters] = useState(props.home.p2.chapterTitles);
 
     useEffect(() => {
-        function introAnimation(elememnt, direction) {
-            direction = direction | 1;
+        let bodyScrollBar = Scrollbar.init(document.body, {
+            damping: 0.1,
+            delegateTo: document,
+        });
 
-            let x = 0;
-            let y = direction * 100;
+        ScrollTrigger.scrollerProxy(".hpage", {
+            scrollTop(value) {
+                if (arguments.length) {
+                    bodyScrollBar.scrollTop = value;
+                }
+                return bodyScrollBar.scrollTop;
+            },
+        });
+        bodyScrollBar.addListener(ScrollTrigger.update);
+        ScrollTrigger.defaults({ scroller: document.querySelector('.hpage') });
 
-            gsap.fromTo(elememnt, { x: x, y: y, autoAlpha: 0 }, {
-                duration: 1.25,
-                x: 0,
-                y: 0,
-                autoAlpha: 1,
-                ease: "expo",
-                overwrite: "auto"
-            });
+        function p1_text() {
+            gsap.from(".reveal1", { duration: 1.5, opacity: 0 });
+            gsap.from(".reveal2", { delay: 0.5, duration: 2, opacity: 0 });
+            gsap.from(".reveal3", { delay: 0.8, duration: 2, opacity: 0 });
+            gsap.from(".reveal4", { delay: 1.1, duration: 2, opacity: 0 });
+            gsap.from(".reveal5", { delay: 1.3, duration: 2, opacity: 0 });
 
         }
-        // gsap.from(".home_p1", {duration: 5, opacity: 0, ease: "elastic"}) 
-        gsap.from(".home_p1", {
-            scrollTrigger: {
-                trigger: ".p1Trigger",
-                onEnter: () => { introAnimation(".home_p1")},
-                onEnterBack: () => { introAnimation(".home_p1", -1)},
-            }
-        })
+        p1_text();
 
-        gsap.from(".home_p2", {
+        gsap.to('.home_p1', {
+            scrollTrigger: {
+                trigger: ".home_p1",
+                start: "top top",
+                end: "bottom 35%",
+                pin: true,
+                scrub: true,
+                // onEnterBack: () => {p1_text()},
+                // markers: true
+            }
+        });
+
+        gsap.from('.home_p2', {
             opacity: 0, scrollTrigger: {
                 trigger: ".p2Trigger",
-                start: "top bottom-=250",
+                start: "top bottom-=100",
                 scrub: true,
                 markers: true,
             }
         })
 
-        
     })
 
     return (
         <Box id="main" className="main-container">
             <Grid container direction="row" spacing={2} className="hpage">
                 <Grid item container direction="column" xs={12} md={6} className="mainText">
-                    <Grid item container direction="column" className="introText home_p1 lText" justify="center">
-                        <Box className="title reveal">{p1Title}</Box>
-                        <Box className="subTitle reveal">{p1Subtitle}</Box>
-                        <Box className="introP reveal" my={1}>{p1Text1}</Box>
-                        <Box className="introP reveal" my={1}>{p1Text2}</Box>
-                        <Box className="introP reveal " my={1}>{p1Text3}</Box>
-                        <Box className="scroll reveal p1Trigger" my={2}>{scroll}</Box>
-                    </Grid>
+                    <Box className="topPagePin">
+                        <Grid item container direction="column" className="introText home_p1 lText" justify="center">
+                            <Box className="title reveal1">{p1Title}</Box>
+                            <Box className="subTitle reveal1">{p1Subtitle}</Box>
+                            <Box className="introP reveal2" my={1}>{p1Text1}</Box>
+                            <Box className="introP reveal3" my={1}>{p1Text2}</Box>
+                            <Box className="introP reveal4 " my={1}>{p1Text3}</Box>
+                            <Box className="scroll reveal5" my={2}>{scroll}</Box>
+                        </Grid>
+                    </Box>
 
                     <Grid item container direction="column" className="bookTitle home_p2 lText" justify="center">
                         <Grid className="p2title title1 reveal">{p2Title1}</Grid>
@@ -83,15 +98,17 @@ const Home = (props) => {
                         <Grid className="p2title title2 reveal">{p2Sub2}</Grid>
                         <Grid className="p2title title2 reveal">{p2Sub3}</Grid>
                     </Grid>
-
                 </Grid>
                 <Grid item xs={12} md={6} >
-                    <Box className="rContent"></Box>
+                    <Box className="topPagePin">
+                        <Box className="rContent"></Box>
+                    </Box>
                     <Grid item container direction="column" align="center" justify="center" className="rContent chapterList">
                         {chapters.map((chapter, index) => (
                             <Link to={`/${chapter}`}
                                 className="homeChapterLinks reveal"
-                                key={index.toString()}>
+                                key={index.toString()}
+                                onClick={() => props.changeChapter(`${chapter}`)}>
                                 {chapter}.
                             </Link>
                         ))}
